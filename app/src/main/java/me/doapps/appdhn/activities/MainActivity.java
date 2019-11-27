0af35dd466e559d83a90ba3b1876735485d29194
imac006 <warriorerlis24@hotmail.com>
2019-11-27 17:30:32 -0500

    6.8

---
+++ app/src/main/java/me/doapps/appdhn/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package me.doapps.appdhn.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.doapps.appdhn.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
